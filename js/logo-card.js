6e8f4614493a7cc3d5ba2822e6ff033117af6628
klausmikealson021 <infogatmus@gmail.com>
2026-08-28 07:02:47 +0330

    Update logo-card.js

--- js/logo-card.js
+++ js/logo-card.js
@@ -1,1 +1,61 @@
+const logoCard =
+document.querySelector(".logo-card");
 
+
+if(logoCard){
+
+
+logoCard.addEventListener(
+"mousemove",
+(e)=>{
+
+
+const rect =
+logoCard.getBoundingClientRect();
+
+
+
+const x =
+(e.clientX - rect.left)
+/
+rect.width
+-.5;
+
+
+
+const y =
+(e.clientY - rect.top)
+/
+rect.height
+-.5;
+
+
+
+logoCard.style.transform = `
+
+rotateX(${y * -15}deg)
+
+rotateY(${x * 20}deg)
+
+`;
+
+
+
+});
+
+
+
+
+
+logoCard.addEventListener(
+"mouseleave",
+()=>{
+
+
+logoCard.style.transform =
+"rotateX(0deg) rotateY(0deg)";
+
+
+});
+
+}
